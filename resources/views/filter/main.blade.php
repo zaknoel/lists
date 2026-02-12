50c9813d137ff7c9d756ac7708f37f9d36c2f3fa
ZakNoel <softdeveleng@gmail.com>
2026-02-12 13:03:36 +0500

    Update form submission handling in main.blade.php

--- resources/views/filter/main.blade.php
+++ resources/views/filter/main.blade.php
@@ -4,11 +4,13 @@
     </div>
     <div class="dropdown-menu p-3 filter_dropdown" style="border: 1px solid #cecece" aria-labelledby="dropdownMenuButton{{$field->attribute}}"
          >
-        <form style="min-width: 200px" data-name="{{$field->attribute}}" data-type="{{$field->getType()}}-{{$field->componentName()}}">
+        <form style="min-width: 200px" data-name="{{$field->attribute}}"
+              onsubmit="event.preventDefault(); this.querySelector('button[type=submit]').click(); return false;"
+              data-type="{{$field->getType()}}-{{$field->componentName()}}">
             <h6 class="mb-2">{{$field->showLabel()}}:</h6>
             {!! $field->filterContent() !!}
             <div class="text-center mt-3">
-                <button type="button" class="btn btn-sm btn-success" onclick="zakFilter.doFilter(this)">Применить</button>
+                <button type="submit" class="btn btn-sm btn-success" onclick="zakFilter.doFilter(this)">Применить</button>
             </div>
         </form>
     </div>
